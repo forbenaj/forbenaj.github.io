cf4a8945a4745d4b83c46caac198d69f970f2a73
forbenaj <benjaminbustelo@gmail.com>
2023-09-08 07:22:07 -0300

    Update bouncing balls

--- bouncing.js
+++ bouncing.js
@@ -14,10 +14,12 @@ canvasContainer.appendChild(canvas);
 // Create a stage on the canvas
 var stage = new createjs.Stage(canvas);
 
+var playMode = "1P"
+
 // Colors
 const white = "#FFFFFF";
-const blue = "#0000FF";
-const red = "#FF0000";
+const blue = "#000022";
+const red = "#220000";
 
 // Ball class
 class Ball {
@@ -26,99 +28,18 @@ class Ball {
         this.y = y;
         this.radius = radius;
         this.color = color;
-        this.speedX = 0;
-        this.speedY = 0;
+        this.speedX = Math.random()*40-20;
+        this.speedY = Math.random()*40-20;
         this.gravity = 0.5;
         this.bounceFactor = 0.7;
-    }
-
-    update() {
-        this.speedY += this.gravity;
-
-        // Store the current position before potential collision adjustments
-        const oldX = this.x;
-        const oldY = this.y;
-
-        // Update position based on speed
-        this.x += this.speedX;
-        this.y += this.speedY;
-
-        // Collision checks and adjustments
-        if (this.x < this.radius) {
-            this.speedX = Math.abs(this.speedX) * this.bounceFactor;
-            this.x = this.radius;
-        }
-        if (this.x > canvas.width - this.radius) {
-            this.speedX = -Math.abs(this.speedX) * this.bounceFactor;
-            this.x = canvas.width - this.radius;
-        }
-
-        if (this.y < this.radius) {
-            this.speedY = Math.abs(this.speedY) * this.bounceFactor;
-            this.y = this.radius;
-        }
-        if (this.y > canvas.height - this.radius) {
-            this.speedY = -Math.abs(this.speedY) * this.bounceFactor;
-            this.y = canvas.height - this.radius;
-        }
-
-        // If the ball got stuck at the wall, restore the old position
-        if ((this.x === oldX && this.speedX !== 0) || (this.y === oldY && this.speedY !== 0)) {
-            this.x = oldX;
-            this.y = oldY;
-        }
-    }
-
-    draw() {
-        const circle = new createjs.Shape();
-        circle.graphics.beginFill(this.color).drawCircle(this.x, this.y, this.radius);
-        stage.addChild(circle);
-    }
-
-    controls(controls) {
-        const keys = controls.keys;
-
-        if (keys[controls.left]) {
-            this.speedX -= 1;
-        }
-        if (keys[controls.right]) {
-            this.speedX += 1;
-        }
-        if (keys[controls.up]) {
-            this.speedY -= 1;
-        }
-        if (keys[controls.down]) {
-            this.speedY += 1;
-        }
-    }
-}
-
-class BlackBall {
-    constructor(radius, color) {
-        this.x = this.getRandomPosition(radius, canvas.width - radius);
-        this.y = this.getRandomPosition(radius, canvas.height - radius);
-        this.active = true;
-        this.radius = radius;
-        this.color = color;
-        this.speedX = 0;
-        this.speedY = 0;
-        this.gravity = 0.5;
-        this.bounceFactor = 0.7;
-        this.minAnimFactor = 0.8;
-        this.maxAnimFactor = 1.3;
         this.animationSpeed = 0.05; // Adjust this value to control animation speed
-        this.time = 0; // Initialize time
-        this.animFactor = 1
+        this.time = 0;
     }
 
-    getRandomPosition(min, max) {
-        return Math.random() * (max - min) + min;
-    }
-
-    animate() {
-        this.time += this.animationSpeed;
+    animate(min, max) {
+        //this.time += this.animationSpeed;
         // Use a sine function to create a smoother animation
-        this.animFactor = this.minAnimFactor + (Math.sin(this.time) * 0.5 + 0.5) * (this.maxAnimFactor - this.minAnimFactor);
+        return min + (Math.sin(this.time) * 0.5 + 0.5) * (max - min);
     }
     
     update() {
@@ -152,10 +73,104 @@ class BlackBall {
         }
 
         // If the ball got stuck at the wall, restore the old position
-        if ((this.x === oldX && this.speedX !== 0) || (this.y === oldY && this.speedY !== 0)) {
+        /*if ((this.x === oldX && this.speedX !== 0) || (this.y === oldY && this.speedY !== 0)) {
             this.x = oldX;
             this.y = oldY;
+        }*/
+    }
+
+    draw() {
+        const circle = new createjs.Shape();
+        if(playMode=="2P"){
+            circle.graphics.beginFill(this.color).drawCircle(this.x, this.y, this.radius*2*this.animate(0.9, 1.1));
+            this.time += this.animationSpeed;
+            if (this.time >= Math.PI * 2){
+                this.time=0
+            }
         }
+        circle.graphics.beginFill("#FFFFFF").drawCircle(this.x, this.y, this.radius*this.animate(1.1, 0.9));
+        stage.addChild(circle);
+    }
+
+    controls(controls) {
+        const keys = controls.keys;
+
+        if (keys[controls.left]) {
+            this.speedX -= 1;
+        }
+        if (keys[controls.right]) {
+            this.speedX += 1;
+        }
+        if (keys[controls.up]) {
+            this.speedY -= 1;
+        }
+        if (keys[controls.down]) {
+            this.speedY += 1;
+        }
+    }
+}
+
+class BlackBall {
+    constructor(radius, color) {
+        this.x = canvas.width / 2 -40;
+        this.y = canvas.height / 2 -33;
+        this.active = true;
+        this.radius = radius;
+        this.color = color;
+        this.speedX = 0;
+        this.speedY = 0;
+        this.gravity = 0.5;
+        this.bounceFactor = 0.7;
+        this.animationSpeed = 0.05; // Adjust this value to control animation speed
+        this.time = 0; // Initialize time
+        this.animFactor = 1
+    }
+
+    getRandomPosition(min, max) {
+        return Math.random() * (max - min) + min;
+    }
+
+    animate(min, max) {
+        //this.time += this.animationSpeed;
+        // Use a sine function to create a smoother animation
+        return min + (Math.sin(this.time) * 0.5 + 0.5) * (max - min);
+    }
+    
+    update() {
+        this.speedY += this.gravity;
+
+        // Store the current position before potential collision adjustments
+        const oldX = this.x;
+        const oldY = this.y;
+
+        // Update position based on speed
+        this.x += this.speedX;
+        this.y += this.speedY;
+
+        // Collision checks and adjustments
+        if (this.x < this.radius) {
+            this.speedX = Math.abs(this.speedX) * this.bounceFactor;
+            this.x = this.radius;
+        }
+        if (this.x > canvas.width - this.radius) {
+            this.speedX = -Math.abs(this.speedX) * this.bounceFactor;
+            this.x = canvas.width - this.radius;
+        }
+
+        if (this.y < this.radius) {
+            this.speedY = Math.abs(this.speedY) * this.bounceFactor;
+            this.y = this.radius;
+        }
+        if (this.y > canvas.height - this.radius) {
+            this.speedY = -Math.abs(this.speedY) * this.bounceFactor;
+            this.y = canvas.height - this.radius;
+        }
+
+        // If the ball got stuck at the wall, restore the old position
+        /*if ((this.x === oldX && this.speedX !== 0) || (this.y === oldY && this.speedY !== 0)) {
+            this.x = oldX;
+            this.y = oldY;
+        }*/
     }
 
 
@@ -168,11 +183,15 @@ class BlackBall {
     draw() {
         if (this.active) {
             const circle = new createjs.Shape();
-            circle.graphics.beginFill("#111111").drawCircle(this.x, this.y, this.radius*1.8*this.animFactor);
-            circle.graphics.beginFill("#222222").drawCircle(this.x, this.y, this.radius*1.5*this.animFactor);
-            circle.graphics.beginFill("#666666").drawCircle(this.x, this.y, this.radius*this.animFactor);
-            circle.graphics.beginFill(this.color).drawCircle(this.x, this.y, this.radius/2);
+            circle.graphics.beginFill("#111111").drawCircle(this.x, this.y, this.radius*1.8*this.animate(0.8, 1.5));
+            circle.graphics.beginFill("#222222").drawCircle(this.x, this.y, this.radius*1.5*this.animate(0.8, 1.3));
+            circle.graphics.beginFill("#666666").drawCircle(this.x, this.y, this.radius*this.animate(0.8, 1.2));
+            circle.graphics.beginFill(this.color).drawCircle(this.x, this.y, (this.radius/2)*this.animate(1.1, 0.9));
             stage.addChild(circle);
+            this.time += this.animationSpeed;
+            if (this.time >= Math.PI * 2){
+                this.time=0
+            }
         }
     }
 
@@ -216,9 +235,10 @@ function graduallyDisappear(elementId, duration) {
   }
 
 
+
 // Initialize the game
-const player1 = new Ball(canvas.width / 2 + 20, canvas.height / 2, 18, white);
-const player2 = new Ball(canvas.width / 2 - 20, canvas.height / 2, 20, white);
+const player1 = new Ball(canvas.width / 4 +canvas.width / 2, canvas.height / 2, 20, red);
+const player2 = new Ball(canvas.width / 4, canvas.height / 2, 20, blue);
 const blackBall = new BlackBall(30, white);
 
 player1Score = 0
@@ -293,9 +313,13 @@ window.addEventListener("keyup", function (event) {
 
 createjs.Ticker.framerate = 60;
 createjs.Ticker.on("tick", function () {
-    player1.controls(controlsArrows);
-    player2.controls(controlsWASD);
+    player1.controls(controlsWASD);
+    player2.controls(controlsArrows);
 
+
+    if (playMode == "1P" && controlsArrows.keys[controlsArrows.left]||controlsArrows.keys[controlsArrows.right]||controlsArrows.keys[controlsArrows.up]||controlsArrows.keys[controlsArrows.down]){
+        playMode = "2P"
+    }
 
     if(deltaX >= 1){
         deltaX = 1
@@ -303,12 +327,14 @@ createjs.Ticker.on("tick", function () {
     if(touchdown){
         // Adjust speed based on touch drag
         console.log(deltaX)
-        player2.speedX = player2.speedX + deltaX; // You can adjust the sensitivity by changing the divisor
-        player2.speedY = player2.speedY + deltaY; // You can adjust the sensitivity by changing the divisor
+        player1.speedX += deltaX; // You can adjust the sensitivity by changing the divisor
+        player1.speedY += deltaY; // You can adjust the sensitivity by changing the divisor
     }
 
     player1.update();
-    player2.update();
+    if(playMode=="2P"){
+        player2.update();
+    }
 
     if (blackBall.active) {
         const distance1 = Math.sqrt((player1.x - blackBall.x) ** 2 + (player1.y - blackBall.y) ** 2);
@@ -319,43 +345,50 @@ createjs.Ticker.on("tick", function () {
             player1Score++
         }
 
-        if (distance2 <= player2.radius + blackBall.radius) {
-            blackBall.respawn();
-            player2Score++
+        if(playMode=="2P"){
+            if (distance2 <= player2.radius + blackBall.radius) {
+                blackBall.respawn();
+                player2Score++
+            }
         }
     }
 
-    if(player2Score>=5 && !playingMode){
+    if(player1Score>=5 && !playingMode){
         graduallyDisappear('webpage', 2000);
         playingMode = true
     }
 
     stage.removeAllChildren();
-    blackBall.animate();
-    blackBall.draw();
-    player1.draw();
-    player2.draw();
 
     if(playingMode){
-        score = new createjs.Text(player2Score,"72px Arial","#888888")
+        score = new createjs.Text(player1Score,"172px Arial","#444444")
         score.x = canvas.width / 2
         score.y = canvas.height /2
+        score.textAlign = "center"
+        score.textBaseline = "middle"
         stage.addChild(score)
     }
 
+
+    //blackBall.animate(0.8,1.3);
+    blackBall.draw();
+    player1.draw();
+    if(playMode=="2P"){
+        player2.draw();
+    }
     if(debug){
-    // Create a Graphics object to define the line's properties
-    var graphics = new createjs.Graphics().setStrokeStyle(2).beginStroke("#FF0000");
+        // Create a Graphics object to define the line's properties
+        var graphics = new createjs.Graphics().setStrokeStyle(2).beginStroke("#FF0000");
 
-    // Define the line's starting and ending points
-    graphics.moveTo(touchStartX, touchStartY);
-    graphics.lineTo(touchCurrentX, touchCurrentY);
+        // Define the line's starting and ending points
+        graphics.moveTo(touchStartX, touchStartY);
+        graphics.lineTo(touchCurrentX, touchCurrentY);
 
-    // Create a Shape object to display the line
-    var line = new createjs.Shape(graphics);
+        // Create a Shape object to display the line
+        var line = new createjs.Shape(graphics);
 
-    // Add the line to the stage
-    stage.addChild(line);
+        // Add the line to the stage
+        stage.addChild(line);
     }
 
     stage.update();
