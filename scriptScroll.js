632a78436d6519f1ae812d1cf1b7188c41ab21e4
forbenaj <benjaminbustelo@gmail.com>
2024-04-30 23:00:52 -0300

    scrollIndex

--- scriptScroll.js
+++ scriptScroll.js
@@ -1,7 +1,13 @@
+
+var windowHeight = window.innerHeight
+var currentHeight = window.scrollY
+var pageHeight = document.body.scrollHeight
+
 window.addEventListener('scroll', function() {
-    var windowHeight = window.innerHeight
-    var currentHeight = window.scrollY
-    var pageHeight = document.body.scrollHeight
+    console.clear()
+    console.log(windowHeight)
+    console.log(currentHeight)
+    console.log(pageHeight)
     if ((currentHeight+windowHeight) >= pageHeight) {
         console.log("Reached bottom")
     }
